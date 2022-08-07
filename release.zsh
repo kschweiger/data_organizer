61014b271156c15107cfc53e8919005555b81d85
Korbinian Schweiger <korbinian.schweiger@gmail.com>
2022-08-07 17:51:43 +0200

    chore: Added .bumpversion.cfg and failsave to release script

--- release.zsh
+++ release.zsh
@@ -2,8 +2,18 @@
 
 echo "Bumping Version"
 bump2version $1
+if [[ $? != 0 ]]
+then
+  echo "bump2version failed. Exiting..."
+  exit 1
+fi
 echo "Updating changelog"
 conventional-changelog -i CHANGELOG.md -s -r 2
+if [[ $? != 0 ]]
+then
+  echo "Generating changelog failed. Exiting..."
+  exit 1
+fi
 git add CHANGELOG.md
 git commit -m "doc: Updated CHANGELOG.md"
 echo "Pushing"
